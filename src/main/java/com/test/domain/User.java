be0a50805bbec15ada427d73d29924555deb5299
zwj <hello@zwj.cn>
2022-04-11 10:16:01 +0800

    CRM模块初始提交

---
+++ src/main/java/com/test/domain/User.java
@@ -0,0 +1,36 @@
+package com.test.domain;
+
+/**
+ * @Author: Mr.Zhou
+ * @Description: TODO
+ * @Date: Created in 2022/4/11 9:30
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
